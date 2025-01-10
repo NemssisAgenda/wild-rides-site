4abc3e2fb8e2454c69d16709c4bef1392cb0a7a8
NemssisAgenda <youssef.aguidi.el@gmail.com>
2025-01-10 12:16:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_7VDZi2ZZy', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '62lhpnjnrrea1hfem54f6mg3i1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientSecret: '1q9smmpiuttnusvp8ervgd0jqhdcb5887glnt3tnunl5o0j1l2e3'
+        userPoolClientSecret: '1q9smmpiuttnusvp8ervgd0jqhdcb5887glnt3tnunl5o0j1l2e3',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
