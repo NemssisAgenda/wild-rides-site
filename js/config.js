673c26379c1801549f62b194e69bf1110ba7f4d5
NemssisAgenda <youssef.aguidi.el@gmail.com>
2025-01-10 11:36:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ex3F7Zt23', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3d2jnrcoguht8mspglnbvhg1ji', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
